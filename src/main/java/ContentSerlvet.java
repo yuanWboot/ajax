44dd74b12e8a869120f0248e734785a67899e3f8
zy <1661996964@qq.com>
2022-10-21 12:08:23 +0800

    Initial commit

---
+++ src/main/java/ContentSerlvet.java
@@ -0,0 +1,14 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/content")
+public class ContentSerlvet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
